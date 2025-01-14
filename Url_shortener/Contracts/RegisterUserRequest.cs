69e00922a59a9bd8afcfef0b938f60a5e4b9ca85
Balychev Kiril <mister.balychev@gmail.com>
2025-01-14 15:00:33 +0200

    Implemented  RegisterUserRequest

--- Url_shortener/Contracts/RegisterUserRequest.cs
+++ Url_shortener/Contracts/RegisterUserRequest.cs
@@ -1,6 +1,9 @@
-﻿namespace Url_shortener.Contracts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Url_shortener.Contracts
 {
-    public class RegisterUserRequest
-    {
-    }
+    public record RegisterUserRequest(
+        [Required] string Username,
+        [Required] string Password,
+        [Required] string Email);
 }
