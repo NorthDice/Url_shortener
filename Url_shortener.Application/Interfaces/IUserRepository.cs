07b28965f38e36b1e3b93a3137a3e85f8b047717
Balychev Kiril <mister.balychev@gmail.com>
2025-01-14 15:18:02 +0200

    Added enum Permissions and created folder for enums

--- Url_shortener.Application/Interfaces/IUserRepository.cs
+++ Url_shortener.Application/Interfaces/IUserRepository.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Url_shortener.Logic.Models;
 
 namespace Url_shortener.Application.Interfaces
 {
     public interface IUserRepository
     {
-        Task Add(User user)
+        Task Add(User user);
+        Task<User> GetByEmail(string email);
+        Task<HashSet<Permissions>> GetUserPermissions(Guid userId);
     }
 }
