c888580582c3478bf6c101270f4194734cde587b
Balychev Kiril <mister.balychev@gmail.com>
2025-01-14 15:37:37 +0200

    Implemented PaswordHasher class

---
+++ Url_shortener.Logic/Models/PasswordHasher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BCrypt.Net;
+using System.Threading.Tasks;
+using Url_shortener.Logic.Interfaces;
+
+namespace Url_shortener.Logic.Models
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Generate(string password) => 
+            BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+
+        public bool Verify(string password, string hashedPassword) =>
+            BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
+    }
+}
