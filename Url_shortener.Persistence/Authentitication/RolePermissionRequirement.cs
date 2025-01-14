caf078ea0c999a75f76e0fb8e2ab5e814fcb6d09
Balychev Kiril <mister.balychev@gmail.com>
2025-01-14 17:52:34 +0200

    Added and implemented RolePermissionRequirement

---
+++ Url_shortener.Persistence/Authentitication/RolePermissionRequirement.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Url_shortener.Logic.Enums;
+
+namespace Url_shortener.Persistence.Authentitication
+{
+    public class RolePermissionRequirement : IAuthorizationRequirement
+    {
+        public RolePermissionRequirement(Permissions[] permissions)
+        {
+            Permissions = permissions;
+        }
+
+        public Permissions[] Permissions { get; set; } = [];
+    }
+}
