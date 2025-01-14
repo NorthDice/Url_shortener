0c3032fb7e89ac6262f0e2ce231d330fb646a1a5
Balychev Kiril <mister.balychev@gmail.com>
2025-01-14 16:42:27 +0200

    Implemented ApiExtensions class

--- Url_shortener.Logic/Models/Extensions/ApiExtensions.cs
+++ Url_shortener.Logic/Models/Extensions/ApiExtensions.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Url_shortener.Logic.Enums;
+using Url_shortener.Logic.Models.JWT;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Url_shortener.Logic.Models.Extensions
 {
-    internal class ApiExtensions
+    public static class ApiExtensions
     {
+        public static void AddApiAuthentication(
+            IServiceCollection services,
+            IOptions<JwtOptions> jwtOptions)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.TokenValidationParameters = new()
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Value.SecretKey))
+                    };
+
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            context.Token = context.Request.Cookies["MovieGo"];
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+        }
     }
 }
