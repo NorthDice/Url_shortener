f730c62bd93f531ecb4d3422a410eb498d1b8c99
Balychev Kiril <mister.balychev@gmail.com>
2025-01-14 15:25:47 +0200

    Created entities for DB implementation

---
+++ Url_shortener.Logic/Entities/RolePermissionEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Url_shortener.Logic.Entities
+{
+    internal class RolePermissionEntity
+    {
+    }
+}
