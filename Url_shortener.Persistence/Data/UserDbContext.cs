b391d8071e312f8810f46a851640807d3a4d9df7
Balychev Kiril <mister.balychev@gmail.com>
2025-01-14 17:27:16 +0200

    Implemented DbContext

--- Url_shortener.Persistence/Data/UserDbContext.cs
+++ Url_shortener.Persistence/Data/UserDbContext.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Url_shortener.Logic.Entities;
+using Url_shortener.Persistence.Configurations;
+using Url_shortener.Persistence.Repositories;
 
 namespace Url_shortener.Persistence.Data
 {
-    internal class UserDbContext
+    public class UserDbContext(
+        DbContextOptions<UserDbContext> options,
+        IOptions<AuthorizationOptions> authOptions) : DbContext(options)
     {
+
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<RoleEntity> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+            modelBuilder.ApplyConfiguration(new PermissionConfiguration());
+
+            modelBuilder.ApplyConfiguration(new RolePermissionConfiguration(authOptions.Value));
+
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
