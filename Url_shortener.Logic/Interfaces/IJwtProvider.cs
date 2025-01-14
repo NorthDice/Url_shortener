cc8113e88f3af5de1f6a1451c4664c1d665e2e39
Balychev Kiril <mister.balychev@gmail.com>
2025-01-14 15:28:33 +0200

    Implemeted IJwtProvider interface

---
+++ Url_shortener.Logic/Interfaces/IJwtProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Url_shortener.Logic.Models;
+
+namespace Url_shortener.Logic.Interfaces
+{
+    public interface IJwtProvider
+    {
+        string GenerateJwtToken(User user);
+    }
+}
