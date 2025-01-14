06fbcafc9e782bf0bca006f4612c3dc26d70bbe2
Balychev Kiril <mister.balychev@gmail.com>
2025-01-14 15:27:33 +0200

    Implemented PasswordHasher interface

---
+++ Url_shortener.Logic/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Url_shortener.Logic.Interfaces
+{
+    public interface IPasswordHasher
+    {
+        string Generate(string password);
+        public bool Verify(string password, string hashedPassword);
+    }
+}
