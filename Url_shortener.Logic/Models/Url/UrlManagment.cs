b68006f63b97dc9e2376efa1a18f226c96a774c7
Balychev Kiril <mister.balychev@gmail.com>
2025-01-15 13:12:27 +0200

    Created UrlManagmentClass

---
+++ Url_shortener.Logic/Models/Url/UrlManagment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Url_shortener.Logic.Models.Url
+{
+    public class UrlManagment
+    {
+        public int Id { get; set; }
+        public string OriginalUrl { get; set; } = string.Empty;
+        public string ShortenedUrl { get; set; } = string.Empty;
+    }
+}
