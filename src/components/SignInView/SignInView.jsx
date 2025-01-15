ebb8627ed347d32f8b5e237a8f7637be55ebe1fb
Balychev Kiril <mister.balychev@gmail.com>
2025-01-15 23:22:08 +0200

    Implemented Sign In view

--- src/components/SignInView/SignInView.jsx
+++ src/components/SignInView/SignInView.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import './SignInView.css';
 
 const SignInView = () => {
   return (
-    <div>
-      saddsadsa
-    </div>
+    <div className="sign-in-container">
+    <h1 className="sign-in-title">Sign In</h1>
+    <form className="sign-in-form">
+      <div className="form-group">
+        <label htmlFor="email" className="form-label">Email:</label>
+        <input
+          type="email"
+          id="email"
+          className="form-input"
+          required
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="password" className="form-label">Password:</label>
+        <input
+          type="password"
+          id="password"
+          className="form-input"
+          required
+        />
+      </div>
+
+      <button type="submit" className="form-button">Sign In</button>
+    </form>
+  </div>
   )
 }
 
