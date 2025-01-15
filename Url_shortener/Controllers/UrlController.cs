45a6336fee35fdb673e0a40087b65b38258d0bca
Balychev Kiril <mister.balychev@gmail.com>
2025-01-15 13:58:02 +0200

    Created UrlController

---
+++ Url_shortener/Controllers/UrlController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Url_shortener.Application.Services;
+
+namespace Url_shortener.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UrlController : Controller
+    {
+        private readonly UrlService _urlService;
+
+        public UrlController(UrlService urlService)
+        {
+            _urlService = urlService;
+        }
+
+        
+    }
+}
