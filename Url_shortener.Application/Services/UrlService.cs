6037ab31c2f863a9dfafab6e9dbc81e23b198fa2
Balychev Kiril <mister.balychev@gmail.com>
2025-01-15 13:49:48 +0200

    Implemented IUrlRepository

---
+++ Url_shortener.Application/Services/UrlService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Url_shortener.Application.Interfaces;
+using Url_shortener.Logic.Interfaces;
+using Url_shortener.Logic.Models.Url;
+
+namespace Url_shortener.Application.Services
+{
+    public class UrlService
+    {
+        private readonly IUrlRepository _urlRepository;
+        private readonly IUserRepository _userRepository;
+
+        public UrlService(IUrlRepository urlRepository, IUserRepository userRepository)
+        {
+            _urlRepository = urlRepository;
+            _userRepository = userRepository;
+        }
+
+        public async Task<UrlManagment> AddUrlForUserAsync(string userEmail, string originalUrl, string shortenedUrl)
+        {
+            var user = await _userRepository.GetByEmail(userEmail);
+            if (user == null) throw new Exception("User not found");
+
+            var url = new UrlManagment
+            {
+                OriginalUrl = originalUrl,
+                ShortenedUrl = shortenedUrl,
+                UserId = user.Id,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            return await _urlRepository.AddUrlAsync(url);
+        }
+
+        public async Task<IEnumerable<UrlManagment>> GetUrlsForUserAsync(int userId)
+        {
+            return await _urlRepository.GetUrlsByUserIdAsync(userId);
+        }
+    }
+}
